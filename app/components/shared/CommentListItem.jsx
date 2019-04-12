a5fb5d29d97753a5b6e4105caeb40eb97427f7db
Ryan Hall <hall.ryan.r@gmail.com>
2019-04-12 13:39:02 -0400

    add photos to comment modal

--- app/components/shared/CommentListItem.jsx
+++ app/components/shared/CommentListItem.jsx
@@ -2,15 +2,17 @@ import  React from 'react';
 import CommentCount from "./CommentCount.jsx";
 import UpVoteCountContainer from "./UpVoteCountContainer.js";
 import DownVoteCountContainer from "./DownVoteCountContainer.js";
+import AuthorIconContainer from "../users/AuthorIconContainer.js";
 import "./CommentListItem.scss";
 
 class CommentListItem extends React.Component {
 
   render() {
-    const { body, num_comments, num_upvotes, num_downvotes, id } = this.props.comment;
+    const { body, num_comments, num_upvotes, num_downvotes, id, user_id } = this.props.comment;
     return (
       <li className="comment-list-item">
-        <div className="comment"> {body}</div>
+        <AuthorIconContainer userId={user_id} />
+        <div className="comment">{body}</div>
         <div className="actions">
           <CommentCount resource={"comment"} id={id} num_comments={num_comments} />
           <UpVoteCountContainer resource={"comment"} id={id} num_up_votes={num_upvotes} />
